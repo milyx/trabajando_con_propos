00dc1cea9b28fdcbdce3dab12d7d57a185815905
milagros acosta <milyarias91@gmail.com>
2022-03-25 13:43:11 -0300

    end changes

---
+++ src/App.vue
@@ -0,0 +1,82 @@
+<template>
+  <div id="app">
+    <h1 class="my-5">{{ titulo }}</h1>
+    <span>{{ texto }}</span>
+    <input
+      type="text"
+      placeholder="Ingresa nueva tarea"
+      v-model="nuevaTarea"
+      class="mx-2"
+    />
+    <button v-on:click="crearTarea" id="btn">Crear</button>
+    <h2 class="my-5">{{ subtitulo }}</h2>
+    <ListPage :tarea="tareas" @borrarPadre="borrarPadre" />
+  </div>
+</template>
+
+<script>
+import ListPage from "./components/ListPage.vue";
+export default {
+  name: "App",
+  components: {
+    ListPage,
+  },
+  data() {
+    return {
+      titulo: "Crea una nueva tarea",
+      subtitulo: "Lista",
+      texto: "Tarea",
+      tareas: [
+        {
+          item: "Ir al supermercado",
+        },
+        {
+          item: "Pagar Internet",
+        },
+        {
+          item: "Pagar Netflix",
+        },
+      ],
+      nuevaTarea: "",
+    };
+  },
+  methods: {
+    crearTarea() {
+      this.tareas.push({
+        item: this.nuevaTarea,
+      });
+      this.nuevaTarea = "";
+    },
+    borrarPadre(i) {
+      this.tareas.splice(i, 1);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.list {
+  text-align: left;
+  width: 300px;
+  margin: auto;
+}
+
+.btn {
+  border: 1px solid gray;
+  margin: 5px;
+  width: 90px;
+  height: 35px;
+}
+#btn {
+  border-radius: 10%;
+  border: 5px solid blue;
+}
+</style>
---
+++ src/components/ListPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="list">
+    <ul>
+      <li v-for="(item, i) in tarea" :key="i">
+        {{ i }} - {{ item.item }}
+        <button @click="borrar(i)" class="btn btn-light">Eliminar</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ListPage",
+  props: {
+    tarea: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  methods: {
+    borrar(i) {
+      this.$emit("borrarPadre", i);
+    },
+  },
+};
+</script>
